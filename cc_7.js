c62d4ce712478e35071af3f1548bc81dc4bf3505
Giana Cresta <crestagiana@gmail.com>
2026-06-14 18:50:23 -0400

    added js comments

--- cc_7.js
+++ cc_7.js
@@ -1,10 +1,10 @@
-const input = document.getElementById("headlineInput");
-const btn = document.getElementById("updateButton");
-const headline = document.getElementById("ctaHeadline");
+const input = document.getElementById("headlineInput"); // selects input
+const btn = document.getElementById("updateButton"); // grabs clicked button
+const headline = document.getElementById("ctaHeadline"); // grabs headline that will change
 
 // Updates Button When CLicked
 btn.addEventListener("click", function() {
     if (input.value !== "") {
         headline.textContent = input.value;
     }
-});
+}); // listens for click, checks input box isn't empty, replaces headline with new text
